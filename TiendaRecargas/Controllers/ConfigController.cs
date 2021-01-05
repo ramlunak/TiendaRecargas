55f651a9bf6e208529c47bba0dc8bf2abba99c00
Royber <royber@yardim.com.br>
2021-01-05 11:02:02 -0300

    config

---
+++ TiendaRecargas/Controllers/ConfigController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaRecargas.Controllers
+{
+    public class ConfigController : BaseController
+    {
+        // GET: ConfigController
+        public ActionResult Index()
+        {
+            IsLogged();
+            ViewBag.Cuenta = Logged;
+            return View();
+        }
+    }
+}
