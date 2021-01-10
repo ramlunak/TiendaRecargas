8d55a17d6ef21914839882dbbe96032e3b412b96
Royber <royber@yardim.com.br>
2021-01-09 21:14:04 -0300

    cambiar contrasena

--- TiendaRecargas/Controllers/ConfigController.cs
+++ TiendaRecargas/Controllers/ConfigController.cs
@@ -4,17 +4,51 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TiendaRecargas.Data;
+using TiendaRecargas.Models;
 
 namespace TiendaRecargas.Controllers
 {
     public class ConfigController : BaseController
     {
+        private readonly AppDbContext _context;
+
+        public ConfigController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         // GET: ConfigController
         public ActionResult Index()
         {
-            IsLogged();
-            ViewBag.Cuenta = Logged;
-            return View();
+            IsLogged();           
+            return View(Logged);
         }
+
+        [HttpPost]       
+        public async Task<IActionResult> UpdatePassword(CuentaUpdatePassword cuentaUpdatePassword)
+        {
+            IsLogged();
+            var cuenta = await _context.RT_Cuentas.FindAsync(cuentaUpdatePassword.idCuenta);
+            if (cuenta == null)
+            {
+                PrompErro("No se pudo actualizar la contraseña, entre en contacto con soporte técnico.");
+                return View(cuentaUpdatePassword);
+            }
+            try
+            {
+                cuenta.Password = cuentaUpdatePassword.Password;
+                _context.RT_Cuentas.Update(cuenta);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                NotifyError(ex.ToString());
+                return View(cuentaUpdatePassword);
+            }
+            NotifySuccess($"La contraseña ha sido actualizada para el usuario {cuentaUpdatePassword.Usuario}.");
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
---
+++ TiendaRecargas/wwwroot/js/views/config/index.js
@@ -0,0 +1,16 @@
+﻿
+$(function () {
+
+});
+
+function UpdatePassword() {
+
+    var updatePassword = {
+        idCuenta: $('#inputIdcuenta').val(),
+        Usuario: $('#inputUsuario').val(),
+        Password: $('#inputPassword').val(),
+        ConfirmPassword: $('#inputConfirmPassword').val()
+    }
+
+    console.log(updatePassword);
+}
